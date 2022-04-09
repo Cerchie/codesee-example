d7bfcc68fcf5d5287e81fb09d082ec47e8804d89
Lucia Cerchie <luciacerchie@gmail.com>
2022-04-09 14:40:19 -0700

    add commit

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ function App() {
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                >
-                    L
-                </a>
+                ></a>
             </header>
         </div>
     )
