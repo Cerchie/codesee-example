bb7110501872c0e1994427205f6677fd96151ac7
Lucia Cerchie <luciacerchie@gmail.com>
2022-04-09 14:39:47 -0700

    add commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn ea
+                    Learn e
                 </a>
             </header>
         </div>
